023c780e1880a7872a6ac29ea0c9cfcd694812b8
кирилл Захаров <csgo2003@mail.ru>
2019-05-31 16:02:24 +0600

    initial

---
+++ DomainsPractice/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DomainsPractice
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void CountButtonClick(object sender, RoutedEventArgs e)
+        {
+            var countAppDomain = AppDomain.CreateDomain("CombinatoricsApp");
+            var countAppPath = AppDomain.CurrentDomain.BaseDirectory + "CombinatoricsApp.exe";
+            countAppDomain.ExecuteAssembly(countAppPath);
+            AppDomain.Unload(countAppDomain);
+        }
+
+        private void LoadFileButtonClick(object sender, RoutedEventArgs e)
+        {
+            var loadAppDomain = AppDomain.CreateDomain("LoadFile");
+            var loadAppPath = AppDomain.CurrentDomain.BaseDirectory + "LoadFile.exe";
+            loadAppDomain.ExecuteAssembly(loadAppPath);
+            AppDomain.Unload(loadAppDomain);
+        }
+    }
+}
